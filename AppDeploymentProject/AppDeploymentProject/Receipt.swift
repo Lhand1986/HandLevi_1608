81e89eea66ab18c788019d0afe247f964b820d25
Levi Hand <lhand1986@yahoo.com>
2016-08-09 13:12:03 -0700

    Till screen fleshed out
    Calculation buttons on till screen are designed properly to emulate the
    proper design of the wireframe.

---
+++ AppDeploymentProject/AppDeploymentProject/Receipt.swift
@@ -0,0 +1,18 @@
+//
+//  Receipt.swift
+//  AppDeploymentProject
+//
+//  Created by Levi Hand on 8/9/16.
+//  Copyright © 2016 Levi Hand. All rights reserved.
+//
+
+import Foundation
+
+class Receipt {
+    var receiptItems: [Int] = []
+    var taxes: Int!
+    var subTotal: Int!
+    var total: Int!
+    
+    
+}
